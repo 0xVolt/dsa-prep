da08a93a17228a1e7abed7cbc62359be06baa9b5
Desh Iyer <desh.iyer@gmail.com>
2024-03-01 10:26:57 +0530

    Update code with optimal solution

--- quick-prep/check-prime.py
+++ quick-prep/check-prime.py
@@ -1,3 +1,13 @@
-x = input(int)
+def checkPrime(x):
+    if x < 2:
+        return "Not Prime"
+    
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return "Not Prime"
+        
+    return "Prime"
 
-print(X)
+x = int(input())
+
+print(checkPrime(x))
