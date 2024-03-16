1764cb2909039298dadd923fe53ee1e5a123eaa4
Desh Iyer <desh.iyer@gmail.com>
2024-03-17 01:27:55 +0530

    Update: Test new code

--- quick-prep/test.py
+++ quick-prep/test.py
@@ -1,17 +1,3 @@
-def get(inputString):
-    freq = {}
-    
-    for char in inputString:
-        if char not in freq:
-            freq[char] = 1
-        else:
-            freq[char] += 1
-        
-    for char in freq:
-        if freq[char] == 1:
-            unique = char 
-            break
-        
-    return inputString.index(unique) + 1 
-    
-print(get("statistics"))
+string = 'jasdhflkajshdkjfasjkdf'
+
+print(''.join(sorted(string)))
