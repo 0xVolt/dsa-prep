bd3eaf19b7274682b56d07229f9b749a0ca9cbc5
Desh Iyer <desh.iyer@gmail.com>
2024-04-21 17:26:02 +0530

    Trim whitespaces

--- quick-prep/shuffle-cards.py
+++ quick-prep/shuffle-cards.py
@@ -1,6 +1,6 @@
 def solution(cards, start, target):
     targetIndex = cards.index(target)
-    
+
     if start == targetIndex:
         return 0
     elif abs(targetIndex - start) == 1:
@@ -9,16 +9,16 @@ def solution(cards, start, target):
         forwardDistance = abs(targetIndex - start)
         backwardDistance = abs(start + ((len(cards) - 1) - targetIndex) + 1)
 
-        return forwardDistance if forwardDistance < backwardDistance else backwardDistance 
+        return forwardDistance if forwardDistance < backwardDistance else backwardDistance
 
 def main():
-    cards = ['black', 'grey', 'brown', 'red', 'blue']    
-    
+    cards = ['black', 'grey', 'brown', 'red', 'blue']
+
     start = 3
     target = 'black'
-    
+
     print(solution(cards, start, target))
-    
-    
+
+
 if __name__ == '__main__':
     main()
