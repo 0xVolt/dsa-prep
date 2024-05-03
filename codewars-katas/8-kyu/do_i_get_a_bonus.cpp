c1b9f0d21b17f79a938b1b14818dca8b2fd03089
Desh Iyer <desh.iyer@gmail.com>
2024-05-04 02:52:34 +0530

    Create solution to the first 8 kyu problem on CodeWars

---
+++ codewars-katas/8-kyu/do_i_get_a_bonus.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+string bonus(int salary, bool bonus) {
+    string returnString = "\0"; 
+    if (bonus) {
+    }
+}
+
+int main() {
+    return 0;
+}
